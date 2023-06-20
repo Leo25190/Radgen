d7a112fae6c2dd021b591663cff41be6d1b235d3
Leo Delecroix <ldelecroix@divotiz.com>
2023-06-20 14:11:36 +0200

    Add grid sorting

--- src/Data/RadzenGridOptions.cs
+++ src/Data/RadzenGridOptions.cs
@@ -1,7 +1,9 @@
-﻿namespace Radgen.Data
+namespace Radgen.Data
 {
     public class RadzenGridOptions
     {
         public bool AllowPaging { get; set; }
+
+        public bool AllowSorting { get; set; }
     }
 }
